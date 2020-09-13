722bec045aa51baafac5bb173d8d65c2aa560f40
Matsujo Hibiki <assintates@gmail.com>
2020-09-13 22:39:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ server.on('listening', function () {
     console.log('listening on ws port:' + server.ws.address().port)
 })
 
-server.listen(80, '', '')
+server.listen(8080, '', '')
 
 // listen for individual tracker messages from peers:
 
